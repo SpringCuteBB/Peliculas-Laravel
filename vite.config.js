cd320d378e2697d1cdb2e44155c22a3afec883df
ismael <ismaelvm2005@gmail.com>
2025-01-28 17:07:40 +0100

    Add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,23 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js", "resources/js/dom.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/dom.js",
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/build",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                app: "resources/js/app.js",
+                dom: "resources/js/dom.js",
+                css: "resources/css/app.css",
+            },
+        },
+    },
 });
